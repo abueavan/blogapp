b5465887de0b43b18e6e5696bef6f6f0510f7eb5
abueavan <eavanzhou@gmail.com>
2019-09-14 17:01:55 +0800

    npm project

---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+morgan.token('post', (request, response) => {
+    if(request.method === 'POST' || request.method === 'PUT') {
+      return JSON.stringify(request.body)
+    }
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://fullstack:20190911@cluster0-gg966.mongodb.net/blog-app?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl, { useNewUrlParser:true, useUnifiedTopology: true, useFindAndModify:false, useCreateIndex: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
